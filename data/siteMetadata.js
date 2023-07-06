9af23d1917332bed370bcdad0ae6a2af9e13a483
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-06 23:37:51 +0530

    JS Interview Prep Questions Part-1 Publish

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Sudhakar Writes Blog',
   author: 'Sudhakar J',
   headerTitle: 'SudhakarWrites',
-  // description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
